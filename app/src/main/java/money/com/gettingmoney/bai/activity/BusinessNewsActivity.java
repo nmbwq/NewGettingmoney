e1c25d1ba53322513e611aeb4ca9fa09b33362e8
nmbwq <1763312610@qq.com>
2017-04-20 19:07:37 +0800

    aa

--- app/src/main/java/money/com/gettingmoney/bai/activity/BusinessNewsActivity.java
+++ app/src/main/java/money/com/gettingmoney/bai/activity/BusinessNewsActivity.java
@@ -68,7 +68,7 @@ public class BusinessNewsActivity extends BaseActivity /*implements OnActionList
     private int xiala;
     private int type;
 
-
+ //shdfsjfksfhskhfskfhskfhsk
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
